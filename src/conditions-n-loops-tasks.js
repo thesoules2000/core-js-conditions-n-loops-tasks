94a982beb6318cad6fb1ef7bc34d52174af0226c
Василевский Максим Евгеньевич <mvasilevsky@mapsoft.by>
2024-12-18 11:31:21 +0300

    feat: getMaxNumber() done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,14 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let maxNumber = a;
+  if (b > a) {
+    maxNumber = b;
+  } else if (c > a) {
+    maxNumber = c;
+  }
+  return maxNumber;
 }
 
 /**
