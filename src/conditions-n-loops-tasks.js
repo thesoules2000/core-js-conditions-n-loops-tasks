e6bd05957009c0d166a9ad61e97cfc7cd6b03fd1
Василевский Максим Евгеньевич <mvasilevsky@mapsoft.by>
2024-12-18 12:01:48 +0300

    feat: canQueenCaptureKing() done

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -66,8 +66,26 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (
+    queen.x < 1 &&
+    queen.x > 8 &&
+    queen.y < 1 &&
+    queen.y > 8 &&
+    king.y < 1 &&
+    king.y > 8 &&
+    king.x < 1 &&
+    king.x > 8
+  ) {
+    return false;
+  }
+  if (queen.x === king.x) {
+    return true;
+  }
+  if (queen.y === king.y) {
+    return true;
+  }
+  return Math.abs(queen.x - king.x) === Math.abs(queen.y - king.y);
 }
 
 /**
