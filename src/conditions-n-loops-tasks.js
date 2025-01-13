5b2465363eeabf3b438246a8eb1b69568ac2ad8d
Maksim <maksim.vasileuskij@gmail.com>
2025-01-14 01:41:15 +0300

    feat: done with isContainNumber()

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -264,8 +264,18 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let number = num;
+
+  while (number > 0) {
+    const lastDigit = number % 10;
+    if (lastDigit === digit) {
+      return true;
+    }
+    number = Math.floor(number / 10);
+  }
+
+  return false;
 }
 
 /**
